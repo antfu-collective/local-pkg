d38c2e3de781f18392b0c1336e5bfa04f27be8b5
chizukicn <chizukicn@outlook.com>
2023-10-01 03:00:43 +0800

    chore: fix test

--- test/ts.test.ts
+++ test/ts.test.ts
@@ -1,11 +1,9 @@
 import { join } from 'path'
-import { existsSync, promises as fs } from 'fs'
+import { promises as fs } from 'fs'
 import { expect, test } from 'vitest'
 import { getPackageInfo, importModule, isPackageExists, loadPackageJSON, resolveModule } from '../src/index'
 
 test('test by typescript', async () => {
-  expect(existsSync('/Users/chizuki/Documents/forks/local-pkg/node_modules/.pnpm/unbuild@2.0.0_typescript@5.1.6/node_modules/unbuild/package.json')).toBe(true)
-
   expect(resolveModule('@antfu/utils')).to.contain(join('node_modules', '@antfu', 'utils'))
 
   expect(isPackageExists('unbuild')).to.eq(true)
